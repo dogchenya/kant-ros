0fa791e59585855a8d7c78b8e3e381a1cd60ae2d
xuchen <xuchen@gs-robot.com>
2023-03-02 15:50:28 +0800

    add vs-project

--- KantFramework/KantNode/main.cpp
+++ KantFramework/KantNode/main.cpp
@@ -3,17 +3,16 @@
 using namespace kant;
 
 /**
-* @brief 配置文件异常类
-* @brief Profile Exception Class
-*/
-struct KT_Cgi_Exception : public KT_Exception
-{
-    KT_Cgi_Exception(const string &buffer) : KT_Exception(buffer){};
-    KT_Cgi_Exception(const string &buffer, int err) : KT_Exception(buffer, err){};
-    ~KT_Cgi_Exception() throw(){};
+ * @brief 配置文件异常类
+ * @brief Profile Exception Class
+ */
+struct KT_Cgi_Exception : public KT_Exception {
+  KT_Cgi_Exception(const string &buffer) : KT_Exception(buffer){};
+  KT_Cgi_Exception(const string &buffer, int err) : KT_Exception(buffer, err){};
+  ~KT_Cgi_Exception() throw(){};
 };
 
 int main(int argc, char *argv[]) {
-    THROW_EXCEPTION_SYSCODE(KT_Cgi_Exception, "parseFormData error!");
-    return 0;
+  THROW_EXCEPTION_SYSCODE(KT_Cgi_Exception, "parseFormData error!");
+  return 0;
 }
